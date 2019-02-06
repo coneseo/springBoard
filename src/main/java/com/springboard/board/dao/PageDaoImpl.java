647e05b9745e6b0c87256d4b2b196f5ce88e3c3d
coneseo <woon4754@gmail.com>
2019-02-06 20:02:15 +0900

    add codes for pagination

--- src/main/java/com/springboard/board/dao/PageDaoImpl.java
+++ src/main/java/com/springboard/board/dao/PageDaoImpl.java
@@ -1,13 +1,37 @@
 package com.springboard.board.dao;
 
 import com.springboard.board.dto.Board;
+import com.springboard.board.dto.Page;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static com.springboard.board.dao.PageDaoSQL.ALLCOUNT;
+
+@Repository
 public class PageDaoImpl implements PageDao {
+    private SimpleJdbcInsert simpleJdbcInsert;
+    private NamedParameterJdbcTemplate jdbc;
+    private RowMapper<Page> rowMapper = BeanPropertyRowMapper.newInstance(Page.class);
+
+    public PageDaoImpl(DataSource dataSource){
+        jdbc = new NamedParameterJdbcTemplate(dataSource);
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                            .withTableName("board")
+                            .usingGeneratedKeyColumns("id");
+    }
     @Override
     public int getCount() {
-        return 0;
+        int result = 0;
+        result = jdbc.queryForObject(ALLCOUNT, (HashMap) null, Integer.class);
+        return result;
     }
 
     @Override
---
+++ src/main/java/com/springboard/board/dto/Page.java
@@ -0,0 +1,12 @@
+package com.springboard.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Page {
+    int count;
+    int startnum;
+    int lastnum;
+}
